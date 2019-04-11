c1bb0c3798d0d00d527b25b55daae496ea0afc7f
akramhelil <akramhelil@gmail.com>
2019-04-11 18:56:23 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+function superbowlWin(array) {
+  let res = array.find(item => item.result === "W"  )
+  if (res) {
+    return res.year;
+  }
+}
